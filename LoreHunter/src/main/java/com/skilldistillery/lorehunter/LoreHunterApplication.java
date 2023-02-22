1732877d367ab4ac6dc9860b8e9e766f09819018
Michael Harrington <mdharr7@gmail.com>
2023-02-22 11:26:24 +0800

    Stub Bott/REST project

---
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/LoreHunterApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.lorehunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoreHunterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoreHunterApplication.class, args);
+	}
+
+}
