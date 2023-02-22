cbe705cf0186aead84dfa085be3b701d8a872e35
Michael Harrington <mdharr7@gmail.com>
2023-02-22 11:35:20 +0800

    Add security config to boot project for password encryption

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/LoreHunterApplication.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/LoreHunterApplication.java
@@ -2,12 +2,20 @@ package com.skilldistillery.lorehunter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LoreHunterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoreHunterApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+	   return new BCryptPasswordEncoder();
 	}
 
 }
