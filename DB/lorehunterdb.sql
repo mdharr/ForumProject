d9dddcee44b8cb2306055512e98ed01951e49868
Michael Harrington <mdharr7@gmail.com>
2023-02-27 14:27:05 +0800

    Remap foreign keys between tables after refactor table and column names

--- DB/lorehunterdb.sql
+++ DB/lorehunterdb.sql
@@ -28,37 +28,38 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` VARCHAR(45) NULL,
   `first_name` VARCHAR(100) NULL,
   `last_name` VARCHAR(100) NULL,
-  `email` VARCHAR(254) NULL,
+  `email` VARCHAR(254) NOT NULL,
   `created_at` TIMESTAMP NULL,
   `image_url` TEXT NULL,
   `last_activity` TIMESTAMP NULL,
   `status` VARCHAR(100) NULL,
-  `post_count` INT NULL,
+  `comment_count` INT NULL,
   `banner_message` VARCHAR(100) NULL,
-  `topic_count` INT NULL,
+  `post_count` INT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `forum`
+-- Table `category`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `forum` ;
+DROP TABLE IF EXISTS `category` ;
 
-CREATE TABLE IF NOT EXISTS `forum` (
+CREATE TABLE IF NOT EXISTS `category` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(1000) NOT NULL,
   `description` VARCHAR(1000) NULL,
   `created_at` TIMESTAMP NULL,
   `status` VARCHAR(100) NULL,
   `view_count` INT NULL,
-  `topic_count` INT NULL,
   `post_count` INT NULL,
+  `comment_count` INT NULL,
   `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_forum_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_forum_user1`
+  INDEX `fk_category_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_category_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
@@ -67,30 +68,29 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `topic`
+-- Table `comment`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `topic` ;
+DROP TABLE IF EXISTS `comment` ;
 
-CREATE TABLE IF NOT EXISTS `topic` (
+CREATE TABLE IF NOT EXISTS `comment` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `subject` VARCHAR(1000) NULL,
-  `created_at` TIMESTAMP NULL,
+  `content` TEXT NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
   `status` VARCHAR(100) NULL,
   `last_edited` TIMESTAMP NULL,
-  `post_count` INT NULL,
+  `comment_id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  `forum_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_thread_user_idx` (`user_id` ASC),
-  INDEX `fk_thread_forum1_idx` (`forum_id` ASC),
-  CONSTRAINT `fk_thread_user`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
+  INDEX `fk_comment_comment1_idx` (`comment_id` ASC),
+  INDEX `fk_comment_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_comment_comment1`
+    FOREIGN KEY (`comment_id`)
+    REFERENCES `comment` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_thread_forum1`
-    FOREIGN KEY (`forum_id`)
-    REFERENCES `forum` (`id`)
+  CONSTRAINT `fk_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -103,30 +103,31 @@ DROP TABLE IF EXISTS `post` ;
 
 CREATE TABLE IF NOT EXISTS `post` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `content` TEXT NOT NULL,
-  `created_at` TIMESTAMP NOT NULL,
+  `subject` VARCHAR(1000) NULL,
+  `created_at` TIMESTAMP NULL,
   `status` VARCHAR(100) NULL,
-  `user_id` INT NOT NULL,
-  `topic_id` INT NOT NULL,
   `last_edited` TIMESTAMP NULL,
-  `post_id` INT NOT NULL,
+  `comment_count` INT NULL,
+  `category_id` INT NOT NULL,
+  `comment_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
+  INDEX `fk_post_category1_idx` (`category_id` ASC),
+  INDEX `fk_post_comment1_idx` (`comment_id` ASC),
   INDEX `fk_post_user1_idx` (`user_id` ASC),
-  INDEX `fk_post_thread1_idx` (`topic_id` ASC),
-  INDEX `fk_post_post1_idx` (`post_id` ASC),
+  CONSTRAINT `fk_post_category1`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_comment1`
+    FOREIGN KEY (`comment_id`)
+    REFERENCES `comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_post_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_post_thread1`
-    FOREIGN KEY (`topic_id`)
-    REFERENCES `topic` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_post_post1`
-    FOREIGN KEY (`post_id`)
-    REFERENCES `post` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -147,7 +148,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `lorehunterdb`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `email`, `created_at`, `image_url`, `last_activity`, `status`, `post_count`, `banner_message`, `topic_count`) VALUES (1, 'admin', '$2a$10$4SMKDcs9jT18dbFxqtIqDeLEynC7MUrCEUbv1a/bhO.x9an9WGPvm', 1, 'ADMIN', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `first_name`, `last_name`, `email`, `created_at`, `image_url`, `last_activity`, `status`, `comment_count`, `banner_message`, `post_count`) VALUES (1, 'admin', '$2a$10$4SMKDcs9jT18dbFxqtIqDeLEynC7MUrCEUbv1a/bhO.x9an9WGPvm', 1, 'ADMIN', NULL, NULL, 'admin@admin.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 COMMIT;
 
