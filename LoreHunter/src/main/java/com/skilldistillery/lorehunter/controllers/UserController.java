e67b40bda62549f2d451d943e3e527717829df19
Michael Harrington <mdharr7@gmail.com>
2023-02-28 20:02:38 +0800

    Stub out post mapping register method for create new user account in user controller

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/UserController.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/UserController.java
@@ -3,12 +3,15 @@ package com.skilldistillery.lorehunter.controllers;
 import java.security.Principal;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +35,22 @@ public class UserController {
 	public User show(Principal principal, @PathVariable String username, HttpServletResponse res) {
 		User user = userService.showByUsername(username);
 		if(user == null) {
+			res.setStatus(404);
+		}
+		return user;
+	}
+	
+	@PostMapping("users")
+	public User register(@RequestBody User user, HttpServletResponse res, HttpServletRequest req) {
+		
+		try {
+			userService.register(user);
+			res.setStatus(201);
+			StringBuffer url = req.getRequestURL();
+			url.append("/").append(user.getId());
+			res.setHeader("Location", url.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
 			res.setStatus(404);
 		}
 		return user;
