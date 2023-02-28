98b08d98ab2cff6aefc3f901e1495345950a6a9d
Michael Harrington <mdharr7@gmail.com>
2023-02-28 20:06:20 +0800

    Stub out put mapping user update method for updating a standard user account

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/UserController.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,22 @@ public class UserController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			res.setStatus(404);
+		}
+		return user;
+	}
+	
+	@PutMapping("users/")
+	public User userUpdate(Principal principal, @RequestBody User user, HttpServletResponse res) {
+		try {
+			user = userService.updateOwn(principal.getName(), user);
+			
+			if(user == null) {
+				res.setStatus(404);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+			user = null;
 		}
 		return user;
 	}
