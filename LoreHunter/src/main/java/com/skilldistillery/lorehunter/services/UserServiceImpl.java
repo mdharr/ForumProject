5bda058bc7d6ee912541680997b1b9991baa6467
Michael Harrington <mdharr7@gmail.com>
2023-02-28 19:30:00 +0800

    Stub out user service impl methods and create logic for index, show, register, and show by username

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/services/UserServiceImpl.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/services/UserServiceImpl.java
@@ -1,32 +1,41 @@
 package com.skilldistillery.lorehunter.services;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.skilldistillery.lorehunter.entities.User;
+import com.skilldistillery.lorehunter.repositories.UserRepository;
 
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository userRepo;
 
 	@Override
 	public List<User> index() {
-		// TODO Auto-generated method stub
-		return null;
+		return userRepo.findAll();
 	}
 
 	@Override
 	public User show(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = null;
+		Optional<User> userOpt = userRepo.findById(id);
+		if(userOpt.isPresent()) {
+			user = userOpt.get();
+		}
+		return user;
 	}
 
 	@Override
 	public User register(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		user.setEnabled(true);
+		return userRepo.saveAndFlush(user);
 	}
 
 	@Override
 	public User updateAdmin(int id, User user) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -50,8 +59,12 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User showByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = null;
+		User userOpt = userRepo.findByUsername(username);
+		if(userOpt != null) {
+			user = userOpt;
+		}
+		return user;
 	}
 
 	@Override
