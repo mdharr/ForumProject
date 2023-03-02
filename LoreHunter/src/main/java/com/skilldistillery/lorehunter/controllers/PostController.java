10ccaaea3af9d9afa15c49fe0bf374a42d801109
Michael Harrington <mdharr7@gmail.com>
2023-03-02 10:15:40 +0800

    Create put mapping in post controller for updating an existing post

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/PostController.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,23 @@ public class PostController {
 			StringBuffer url = req.getRequestURL();
 			url.append("/").append(post.getId());
 			res.setHeader("Location", url.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+			post = null;
+		}
+		
+		return post;
+	}
+	
+	@PutMapping("categories/{cid}/posts/{pid}")
+	public Post update(Principal principal, HttpServletRequest req, HttpServletResponse res, @PathVariable("pid") int postId, @PathVariable("cid") int categoryId, @RequestBody Post post) {
+		
+		try {
+			post = postService.update(principal.getName(), postId, post, categoryId);
+			if(post == null) {
+				res.setStatus(404);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			res.setStatus(400);
