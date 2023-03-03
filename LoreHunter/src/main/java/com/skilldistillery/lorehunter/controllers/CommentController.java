6a7236331c52bf97b09021cc6656cc5a99b2b55a
Michael Harrington <mdharr7@gmail.com>
2023-03-03 11:56:12 +0800

    Working through fix for 405 error post method not supported

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/CommentController.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/CommentController.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.lorehunter.entities.Comment;
+import com.skilldistillery.lorehunter.entities.Post;
 import com.skilldistillery.lorehunter.services.CommentService;
 
 @CrossOrigin({ "*", "http://localhost/"})
@@ -27,6 +30,24 @@ public class CommentController {
 	@GetMapping("categories/{cid}/posts/{pid}/comments")
 	public List<Comment> index(Principal principal, HttpServletRequest req, HttpServletResponse res, @PathVariable("cid") int categoryId, @PathVariable("pid") int postId) {
 		return commentService.index(postId, categoryId);
+	}
+	
+	@PostMapping("categories/{cid}/posts{pid}/comments")
+	public Comment create(Principal principal, HttpServletRequest req, HttpServletResponse res, @PathVariable("cid") int categoryId, @PathVariable("pid") int postId, @RequestBody Comment comment) {
+		
+		try {
+			commentService.create(principal.getName(), comment, postId);
+			res.setStatus(201);
+			StringBuffer url = req.getRequestURL();
+			url.append("/").append(comment.getId());
+			res.setHeader("Location", url.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+			comment = null;
+		}
+		
+		return comment;
 	}
 
 }
