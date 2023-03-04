9413143d5a8ddcab31732e3889bc0696c1d40ee0
Michael Harrington <mdharr7@gmail.com>
2023-03-04 11:12:24 +0800

    Stub out fields and constructor with fields for category model in Angular

--- ngLoreHunter/src/app/models/category.ts
+++ ngLoreHunter/src/app/models/category.ts
@@ -1,2 +1,42 @@
+import { Post } from "./post";
+import { User } from "./user";
+
 export class Category {
+  id: number;
+  name: string;
+  description: string;
+  createdAt: string;
+  status: string;
+  viewCount: number;
+  postCount: number;
+  commentCount: number;
+  enabled: boolean;
+  user: User;
+  posts: Post[];
+
+  constructor(
+    id: number = 0,
+    name: string = '',
+    description: string = '',
+    createdAt: string = '',
+    status: string = '',
+    viewCount: number = 0,
+    postCount: number = 0,
+    commentCount: number = 0,
+    enabled: boolean = false,
+    user: User = new User(),
+    posts: Post[] = []
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.createdAt = createdAt;
+    this.status = status;
+    this.viewCount = viewCount;
+    this.postCount = postCount;
+    this.commentCount = commentCount;
+    this.enabled = enabled;
+    this.user = user;
+    this.posts = posts;
+  }
 }
