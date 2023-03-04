ee11f0e830b1ad2aae13cc3157be397006177e73
Michael Harrington <mdharr7@gmail.com>
2023-03-04 11:32:27 +0800

    Stub out fields and constructor with fields for post model in Angular

--- ngLoreHunter/src/app/models/comment.ts
+++ ngLoreHunter/src/app/models/comment.ts
@@ -1,2 +1,39 @@
+import { Post } from "./post";
+import { User } from "./user";
+
 export class Comment {
+  id: number;
+  content: string;
+  createdAt: string;
+  status: string;
+  lastEdited: string;
+  enabled: boolean;
+  user: User;
+  post: Post;
+  commentId: number;
+  parentComment: Comment;
+
+  constructor(
+    id: number = 0,
+    content: string = '',
+    createdAt: string = '',
+    status: string = '',
+    lastEdited: string = '',
+    enabled: boolean = false,
+    user: User = new User(),
+    post: Post = new Post(),
+    commentId: number = 0,
+    parentComment: Comment = new Comment()
+  ) {
+    this.id = id;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.status = status;
+    this.lastEdited = lastEdited;
+    this.enabled = enabled;
+    this.user = user;
+    this.post = post;
+    this.commentId = commentId;
+    this.parentComment = parentComment;
+  }
 }
