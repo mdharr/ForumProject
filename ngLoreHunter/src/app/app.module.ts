8b95a23e69d6a499278ee284a335277791b9967b
Michael Harrington <mdharr7@gmail.com>
2023-03-04 11:37:28 +0800

    Create about component for application description

--- ngLoreHunter/src/app/app.module.ts
+++ ngLoreHunter/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
