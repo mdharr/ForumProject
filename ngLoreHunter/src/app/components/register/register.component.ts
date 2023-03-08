6dcf74efa165602efaac64bbb1f623f617052080
Michael Harrington <mdharr7@gmail.com>
2023-03-08 10:41:15 +0800

    Complete register component boilerplate, styling, and register function logic

--- ngLoreHunter/src/app/components/register/register.component.ts
+++ ngLoreHunter/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  newUser: User = new User();
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private modalService: NgbModal) {}
+
+  register(newUser: User): void {
+    console.log('Registering user:');
+    console.log(newUser);
+    this.authService.register(newUser).subscribe({
+      next: (registeredUser) => {
+        this.authService.login(newUser.username, newUser.password).subscribe({
+          next: (loggedInUser) => {
+            this.modalService.dismissAll();
+            this.router.navigateByUrl('/home');
+          },
+          error: (problem) => {
+            console.error('RegisterComponent.register(): Error logging in user:');
+            console.error(problem);
+          }
+        });
+      },
+      error: (fail) => {
+        console.error('RegisterComponent.register(): Error registering account');
+        console.error(fail);
+      }
+    });
+  }
 
 }
