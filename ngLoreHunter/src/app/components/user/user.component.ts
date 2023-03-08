1acdb497080881cdb45c549f6377e0c7ab82bb80
Michael Harrington <mdharr7@gmail.com>
2023-03-08 10:55:50 +0800

    Complete user fields in user model

--- ngLoreHunter/src/app/components/user/user.component.ts
+++ ngLoreHunter/src/app/components/user/user.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  private url = environment.baseUrl + 'api/users';
+
+  selected: null | User = null;
+  userList: User[] = [];
+  newUser: User = new User();
+  editUser: User | null = null;
+  user: User | null = null;
+  comments: Comment[] = [];
+
+  constructor(
+    private userService: UserService,
+    // private categoryService: CategoryService,
+    private http: HttpClient,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    // private postService: PostService,
+    // private commentService: CommentService
+  ) {}
+
+  ngOnInit(): void {
+    let idString = this.route.snapshot.paramMap.get('userId');
+    if (idString) {
+      console.log('******************');
+
+      console.log('userId: ' + idString);
+      let userId = Number(idString);
+      if (!isNaN(userId)) {
+        this.userService.show(userId).subscribe({
+          next: (user) => {
+            console.log('user: ' + user);
+            console.log(user.id);
+            console.log('ngOnInit method in user');
+            this.selected = user;
+          },
+          error: (fail) => {
+            console.error(fail);
+            this.router.navigateByUrl('UserNotFound');
+          },
+        });
+      } else {
+        this.router.navigateByUrl('invalidUserId');
+      }
+    }
+    this.reload();
+  }
+
+  reload() {
+    this.userService.index().subscribe({
+      next: (userList) => {
+        this.userList = userList;
+      },
+      error: (err) => {
+        console.error('Error loading user list: ');
+        console.error(err);
+      },
+    });
+  }
+
+  displayUser(user: User) {
+    this.selected = user;
+  }
+
+  displayTable() {
+    this.selected = null;
+  }
+
+  setEditUser() {
+    this.editUser = Object.assign({}, this.selected);
+  }
+
+  updateUserAdmin(user: User, id: number): void {
+    console.log('in editInformation');
+    this.userService.disableAdmin(user).subscribe({
+      next: (user) => {
+        user.enabled = false;
+        this.reload();
+      },
+      error: (error) => {
+        console.log('Error getting loggedInUser Profile Component');
+        console.log(error);
+      },
+    });
+  }
+
+  // getCommentsForUser() {
+  //   this.commentService.userCommentIndex(this.selected!.id).subscribe({
+  //     next: (comments) => {
+  //       this.comments = comments;
+  //     },
+  //     error: (err) => {
+  //       console.error(err);
+  //       console.error('error retrieving comments for user');
+  //     },
+  //   });
+  // }
+
+  deleteUser(id: number) {
+
+  }
 
 }
--- ngLoreHunter/src/app/models/user.ts
+++ ngLoreHunter/src/app/models/user.ts
@@ -1,3 +1,8 @@
+import { Category } from "./category";
+import { Post } from "./post";
+import { Comment } from "./comment";
+import { first } from "rxjs";
+
 export class User {
 
   id: number;
@@ -5,18 +10,55 @@ export class User {
   password: string;
   enabled: boolean;
   role: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  createdAt: string;
+  imageUrl: string;
+  lastActivity: string;
+  status: string;
+  commentCount: number;
+  postCount: number;
+  categories: Category[];
+  posts: Post[];
+  comments: Comment[];
+
 
   constructor(
     id: number = 0,
     username: string = '',
     password: string = '',
     enabled: boolean = false,
-    role: string = ''
+    role: string = '',
+    firstName: string = '',
+    lastName: string = '',
+    email: string = '',
+    createdAt: string = '',
+    imageUrl: string = '',
+    lastActivity: string = '',
+    status: string = '',
+    commentCount: number = 0,
+    postCount: number = 0,
+    categories: Category[] = [],
+    posts: Post[] = [],
+    comments: Comment[] = []
   ) {
     this.id = id;
     this.username = username;
     this.password = password;
     this.enabled = enabled;
     this.role = role;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.createdAt = createdAt;
+    this.imageUrl = imageUrl;
+    this.lastActivity = lastActivity;
+    this.status = status;
+    this.commentCount = commentCount;
+    this.postCount = postCount;
+    this.categories = categories;
+    this.posts = posts;
+    this.comments = comments;
   }
 }
