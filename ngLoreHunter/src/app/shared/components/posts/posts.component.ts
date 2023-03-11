7624ba12bf51390c165a79ab5a3f4be06adfe11e
Michael Harrington <mdharr7@gmail.com>
2023-03-11 13:02:28 +0800

    Create necessary fields for posts component and inject necessary services and routing into constructor

--- ngLoreHunter/src/app/shared/components/posts/posts.component.ts
+++ ngLoreHunter/src/app/shared/components/posts/posts.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from 'src/app/models/post';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { CommentService } from 'src/app/services/comment.service';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,5 +13,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./posts.component.css']
 })
 export class PostsComponent {
+  title = 'ngLoreHunter';
+
+  posts: Post[] = [];
+  post: Post | null = null;
+  users: User[] = [];
+  select: null | Post = null;
+
+  newPost: Post = new Post();
+
+  postsByCategory: Post[] = [];
+
+  comments: Comment[] = [];
+  newComment: Comment = new Comment();
+  loggedInUser: User = new User();
+
+  postCreated = false;
+  showForm: boolean = false;
+  addPostMod: Post | null = null;
+
+  selectedSearch: string = 'all';
+
+  constructor(
+    private postService: PostService,
+    private commentService: CommentService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private categoryService: CategoryService,
+  ) {}
 
 }
