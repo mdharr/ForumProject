1efb7560f4b8ce385cc9fe97977cc5eea9bdaa93
Michael Harrington <mdharr7@gmail.com>
2023-03-22 10:28:37 +0800

    Stub out profile component ts and import all necessary imports to retrieve data for material table. Implement material table to display user comments.

--- ngLoreHunter/src/app/shared/components/profile/profile.component.ts
+++ ngLoreHunter/src/app/shared/components/profile/profile.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { Comment } from 'src/app/models/comment';
+import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { CategoryService } from 'src/app/services/category.service';
+import { CommentService } from 'src/app/services/comment.service';
+import { HomeService } from 'src/app/services/home.service';
+import { PostService } from 'src/app/services/post.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { Subscription } from 'rxjs';
+import { Category } from 'src/app/models/category';
+import { User } from 'src/app/models/user';
+import { PostDataSource } from 'src/app/services/post.dataSource';
+import { CommentDataSource } from 'src/app/services/comment.dataSource';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  title = 'ngLoreHunter';
+
+  public Editor = ClassicEditor;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  displayedColumns: string[] = ['user', 'content'];
+
+  dataSource = new CommentDataSource(this.commentService);
+
+  paramsSub: Subscription | undefined;
+
+  posts: Post[] = [];
+  categories: Category[] = [];
+  post: null | Post = null;
+  users: User[] = [];
+  selected: Post = new Post();
+  categoryId: number = 0;
+  postId: number = 0;
+  value: any;
+
+  newPost: Post = new Post();
+
+  postsByCategory: Post[] = [];
+
+  comments: Comment[] = [];
+  comment: Comment = new Comment();
+  newComment: Comment = new Comment();
+  loggedInUser: User = new User();
+
+  postCreated = false;
+  showForm: boolean = false;
+  addPostMod: Post | null = null;
+
+  selectedSearch: string = 'all';
+
+  content = new FormControl('', [Validators.required]);
+  checkCkEditor: boolean = false;
+
+  constructor(
+    private postService: PostService,
+    private commentService: CommentService,
+    private authService: AuthService,
+    private router: Router,
+    private categoryService: CategoryService,
+    private activatedRoute: ActivatedRoute,
+    private homeService: HomeService,
+    private http: HttpClient
+    ) {}
+
+    ngOnInit() {
+
+    }
+
+
+  sortComments(sort: Sort): void {
+    this.dataSource.loadComments(this.categoryId, this.postId, sort);
+  }
 
 }
