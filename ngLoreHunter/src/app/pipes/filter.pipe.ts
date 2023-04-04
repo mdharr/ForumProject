7524914adcd487a8d48ccb3c38ce53e1727a9a6c
Michael Harrington <mdharr7@gmail.com>
2023-04-04 11:07:26 +0800

    Implement logic for filter pipe which filters based on property name by use of filterString

--- ngLoreHunter/src/app/pipes/filter.pipe.ts
+++ ngLoreHunter/src/app/pipes/filter.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any[], filterString: string, propName: string): any[] {
+    const resultArray = [];
+    if (value.length === 0 || filterString === '' || propName === '') {
+      return value;
+    }
+
+    for (const item of value) {
+      if (item[propName] === filterString) {
+        resultArray.push(item);
+      }
+    }
+    return resultArray;
   }
 
 }
