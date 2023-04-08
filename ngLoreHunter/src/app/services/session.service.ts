fb6e99e5c8a00af7764510f1597f2442bab7820f
Michael Harrington <mdharr7@gmail.com>
2023-04-08 19:14:03 +0800

    Generate new service in angular called SessionService to active keep track of how many sessions are running by making an call to backend api endpoint /sessions/active

---
+++ ngLoreHunter/src/app/services/session.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+  private url = environment.baseUrl + 'api';
+
+  constructor(private http: HttpClient) { }
+
+  getActiveSessionCount(): Observable<number> {
+    return this.http.get<number>(this.url + '/sessions/active');
+  }
+}
