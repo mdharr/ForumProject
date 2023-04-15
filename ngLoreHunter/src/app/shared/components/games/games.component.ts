4bf078eae4fc43af641d02ee72e37b59b95706fd
Michael Harrington <mdharr7@gmail.com>
2023-04-15 11:06:53 +0800

    Add card click and hover events for styling purposes

--- ngLoreHunter/src/app/shared/components/games/games.component.ts
+++ ngLoreHunter/src/app/shared/components/games/games.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 import { map, Subscription } from 'rxjs';
 import { Game } from 'src/app/models/game';
 import { GameService } from 'src/app/services/game.service';
@@ -12,11 +12,12 @@ export class GamesComponent implements OnInit {
 
   games: any[] = [];
 
-  clickedCard: Element | null = null; // Global variable to keep track of clicked card
+  cardClicked = false; // Flag to track if the card is clicked
+  cardElement: HTMLElement | null = null; // Reference to the card element
 
   private subscription: Subscription = new Subscription();
 
-  constructor(private gameService: GameService) { }
+  constructor(private gameService: GameService, private renderer: Renderer2, private el: ElementRef) { }
 
   ngOnInit() {
     this.getGames();
@@ -43,40 +44,48 @@ export class GamesComponent implements OnInit {
     }
   }
 
-  onCardClick(event: Event) {
-    // // Get the clicked element
-    // const clickedElement = event.target as HTMLElement;
+  onCardClick(event: MouseEvent) {
+    const clickedCardElement = event.currentTarget as HTMLElement;
 
-    // // Check if the clicked element has the "item-card" class
-    // if (clickedElement.classList.contains('item-card')) {
-    //   // Remove the "card-clicked" class from any previously clicked item-card
-    //   const previouslyClickedElement = document.querySelector('.item-card.card-clicked');
-    //   if (previouslyClickedElement) {
-    //     previouslyClickedElement.classList.remove('card-clicked');
-    //   }
+    // If a card is already clicked, return it to its original position
+    if (this.cardElement && this.cardElement !== clickedCardElement) {
+      this.cardElement.classList.remove('card-clicked');
+    }
 
-    //   // Add the "card-clicked" class to the clicked item-card
-    //   clickedElement.classList.add('card-clicked');
+    // Set the current clicked card as the new cardElement
+    this.cardElement = clickedCardElement;
+    this.cardElement.classList.add('card-clicked');
+  }
 
-    //   // Center the clicked item-card in the window
-    //   const cardRect = clickedElement.getBoundingClientRect();
-    //   const windowWidth = window.innerWidth || document.documentElement.clientWidth;
-    //   const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    //   const cardLeft = (windowWidth - cardRect.width) / 2;
-    //   const cardTop = (windowHeight - cardRect.height) / 2;
-    //   clickedElement.style.left = cardLeft + 'px';
-    //   clickedElement.style.top = cardTop + 'px';
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    // If a card is clicked and the click is outside of the card, return the card to its original position
+    if (this.cardElement && !this.cardElement.contains(event.target as Node)) {
+      this.cardElement.classList.remove('card-clicked');
+      this.cardElement = null;
+    }
+  }
 
-    //   // Add a click event listener to the document to detect clicks outside of the item-card
-    //   document.addEventListener('click', (event) => {
-    //     const target = event.target as HTMLElement;
-    //     // Check if the clicked element is not a child of an item-card
-    //     if (!target.closest('.item-card')) {
-    //       // Remove the "card-clicked" class from the clicked item-card
-    //       clickedElement.classList.remove('card-clicked');
-    //     }
-    //   });
-    // }
+  onDocumentMouseMove(event: MouseEvent) {
+    // Update the position of the card based on the mouse cursor position
+    if (this.cardElement && this.cardElement.classList.contains('card-clicked')) {
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+      const cardRect = this.cardElement.getBoundingClientRect();
+      const cardX = cardRect.left + cardRect.width / 2;
+      const cardY = cardRect.top + cardRect.height / 2;
+      const offsetX = mouseX - cardX;
+      const offsetY = mouseY - cardY;
+      this.cardElement.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(1.5)`;
+    }
+  }
+
+  @HostListener('document:mouseleave')
+  onDocumentMouseLeave() {
+    // Reset the position and zoom of the card when the mouse leaves the document
+    if (this.cardElement && this.cardElement.classList.contains('card-clicked')) {
+      this.cardElement.style.transform = 'translate(-50%, -50%) scale(1.5)';
+    }
   }
 
 }
