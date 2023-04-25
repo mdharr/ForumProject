6b02551f41a413bd088e10eefbf0ddc50295f5ad
Michael Harrington <mdharr7@gmail.com>
2023-04-25 11:12:50 +0800

    Stub out repositories for notification and user notification

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/repositories/UserNotificationRepository.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/repositories/UserNotificationRepository.java
@@ -1,5 +1,21 @@
 package com.skilldistillery.lorehunter.repositories;
 
-public interface UserNotificationRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.skilldistillery.lorehunter.entities.Notification;
+import com.skilldistillery.lorehunter.entities.User;
+import com.skilldistillery.lorehunter.entities.UserNotification;
+import com.skilldistillery.lorehunter.entities.UserNotificationId;
+
+public interface UserNotificationRepository extends JpaRepository<UserNotification, Integer>{
+	
+	UserNotification findByUserAndNotification(User user, Notification notification);
+	
+	@Query("SELECT u FROM UserNotification u WHERE u.id = :userNotificationId")
+    Optional<UserNotification> findByUserNotificationId(@Param("userNotificationId") UserNotificationId userNotificationId);
 
 }
