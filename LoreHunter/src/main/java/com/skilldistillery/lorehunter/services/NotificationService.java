36cab2d997915b83daa498d1a6804eab52984a7b
Michael Harrington <mdharr7@gmail.com>
2023-04-25 11:24:06 +0800

    Stub out notification service and service implementation methods for basic crud operations

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/services/NotificationService.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/services/NotificationService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.lorehunter.services;
 
+import java.util.List;
+
+import com.skilldistillery.lorehunter.entities.Notification;
+
 public interface NotificationService {
+
+	List<Notification> getAllNotifications();
+
+	Notification getNotificationById(int id);
+
+	Notification createNotification(Notification notification);
+
+	Notification updateNotification(Notification notification);
+
+	void deleteNotificationById(int id);
 
 }
--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/services/NotificationServiceImpl.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/services/NotificationServiceImpl.java
@@ -1,5 +1,40 @@
 package com.skilldistillery.lorehunter.services;
 
-public class NotificationServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.skilldistillery.lorehunter.entities.Notification;
+import com.skilldistillery.lorehunter.repositories.NotificationRepository;
+
+public class NotificationServiceImpl implements NotificationService{
+	
+    @Autowired
+    private NotificationRepository notificationRepo;
+
+    @Override
+    public List<Notification> getAllNotifications() {
+        return notificationRepo.findAll();
+    }
+
+    @Override
+    public Notification getNotificationById(int id) {
+        return notificationRepo.findById(id);
+    }
+
+    @Override
+    public Notification createNotification(Notification notification) {
+        return notificationRepo.save(notification);
+    }
+
+    @Override
+    public Notification updateNotification(Notification notification) {
+        return notificationRepo.save(notification);
+    }
+
+    @Override
+    public void deleteNotificationById(int id) {
+        notificationRepo.deleteById(id);
+    }
 
 }
