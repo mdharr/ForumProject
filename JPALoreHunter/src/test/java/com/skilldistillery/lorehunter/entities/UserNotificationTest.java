fa70b2cb06379f68042af72df02991e622392934
Michael Harrington <mdharr7@gmail.com>
2023-04-25 10:43:36 +0800

    Generate junit test for user notification

---
+++ JPALoreHunter/src/test/java/com/skilldistillery/lorehunter/entities/UserNotificationTest.java
@@ -0,0 +1,40 @@
+package com.skilldistillery.lorehunter.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserNotificationTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
