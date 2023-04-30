c5b4328a82a6e41d7d484d49f47e429e0034732d
Michael Harrington <mdharr7@gmail.com>
2023-04-30 11:25:56 +0800

    Finish stubbing out fields, getters, setters, hashcode, equals, and tostring in like entity class

--- JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/Like.java
+++ JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/Like.java
@@ -1,8 +1,95 @@
 package com.skilldistillery.lorehunter.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Like {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name = "comment_id")
+	private Comment comment;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public Like() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Like(int id, Comment comment, User user) {
+		super();
+		this.id = id;
+		this.comment = comment;
+		this.user = user;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Comment getComment() {
+		return comment;
+	}
+
+	public void setComment(Comment comment) {
+		this.comment = comment;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Like other = (Like) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Like [id=");
+		builder.append(id);
+		builder.append(", comment=");
+		builder.append(comment);
+		builder.append(", user=");
+		builder.append(user);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 
 }
