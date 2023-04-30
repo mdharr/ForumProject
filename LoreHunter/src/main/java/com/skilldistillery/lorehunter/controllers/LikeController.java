0275944be3a1a7843a66b7adae9a717bf4ee4cf9
Michael Harrington <mdharr7@gmail.com>
2023-04-30 12:49:40 +0800

    rest api endpoint for user has liked getmapping test success in postman

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/LikeController.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/LikeController.java
@@ -108,6 +108,7 @@ public class LikeController {
 	    }
 	}
 
+	// rest api endpoint test success in postman
 	@GetMapping("comments/{commentId}/likes/has-liked")
 	public ResponseEntity<Boolean> hasUserLikedComment(
 		    @PathVariable int commentId,
