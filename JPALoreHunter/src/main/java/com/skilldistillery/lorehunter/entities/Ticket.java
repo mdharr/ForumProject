2b3d31eb6a5ab1a63e1b6f50118ed7f718172dd2
Michael Harrington <mdharr7@gmail.com>
2023-05-08 15:45:12 +0800

    Add entity annotation and id for ticket as well as add ticket entity to persistence xml file

--- JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/Ticket.java
+++ JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/Ticket.java
@@ -1,5 +1,15 @@
 package com.skilldistillery.lorehunter.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Ticket {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
 }
