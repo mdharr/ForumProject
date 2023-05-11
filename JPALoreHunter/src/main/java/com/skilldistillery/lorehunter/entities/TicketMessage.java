080a55c3024651a03ad90c9ea0a6bf1278bd81c6
Michael Harrington <mdharr7@gmail.com>
2023-05-11 11:01:46 +0800

    Generate and stub out ticket message entity and add to persistence xml

---
+++ JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/TicketMessage.java
@@ -0,0 +1,125 @@
+package com.skilldistillery.lorehunter.entities;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class TicketMessage {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String content;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@ManyToOne
+	@JoinColumn(name = "ticket_id")
+	private Ticket ticket;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public TicketMessage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public TicketMessage(int id, String content, LocalDateTime createdAt, Ticket ticket, User user) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.ticket = ticket;
+		this.user = user;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Ticket getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TicketMessage other = (TicketMessage) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TicketMessage [id=");
+		builder.append(id);
+		builder.append(", content=");
+		builder.append(content);
+		builder.append(", createdAt=");
+		builder.append(createdAt);
+		builder.append(", ticket=");
+		builder.append(ticket);
+		builder.append(", user=");
+		builder.append(user);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
