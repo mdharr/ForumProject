67a5bc2f5ce61f81c9c5afad4bd3b0840e737df3
Michael Harrington <mdharr7@gmail.com>
2023-05-12 09:23:07 +0800

    Stub out Ticket class in models directory, including definitions for both ticket status and ticket priority enums

--- ngLoreHunter/src/app/models/ticket.ts
+++ ngLoreHunter/src/app/models/ticket.ts
@@ -1,2 +1,50 @@
+import { TicketMessage } from "./ticket-message";
+import { User } from "./user";
+
 export class Ticket {
+  id: number;
+  title: string;
+  description: string;
+  status: TicketStatus;
+  priority: TicketPriority;
+  createdAt: string;
+  updatedAt: string;
+  user: User;
+  ticketMessages: TicketMessage[];
+
+  constructor(
+    id: number = 0,
+    title: string = '',
+    description: string = '',
+    status: TicketStatus = TicketStatus.OPEN,
+    priority: TicketPriority = TicketPriority.MEDIUM,
+    createdAt: string = '',
+    updatedAt: string = '',
+    user: User = new User,
+    ticketMessages: TicketMessage[] = []
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.status = status;
+    this.priority = priority;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this.user = user;
+    this.ticketMessages = ticketMessages;
+  }
+
+}
+
+export enum TicketStatus {
+  OPEN = 'OPEN',
+  IN_PROGRESS = 'IN_PROGRESS',
+  RESOLVED = 'RESOLVED',
+  CLOSED = 'CLOSED'
+}
+
+export enum TicketPriority {
+  LOW = 'LOW',
+  MEDIUM = 'MEDIUM',
+  HIGH = 'HIGH'
 }
