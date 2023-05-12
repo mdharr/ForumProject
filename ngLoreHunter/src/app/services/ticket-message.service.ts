7547b807ef436ad706a966ff0040bb98a9267671
Michael Harrington <mdharr7@gmail.com>
2023-05-12 10:25:52 +0800

    Implement method in frontend ticket message service for creating a new message via http post request to backend rest api by ticket id

--- ngLoreHunter/src/app/services/ticket-message.service.ts
+++ ngLoreHunter/src/app/services/ticket-message.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { TicketMessage } from '../models/ticket-message';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketMessageService {
+  private url = environment.baseUrl + 'api';
 
-  constructor() { }
+  constructor(private authService: AuthService, private http: HttpClient,) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.authService.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  getMessagesByTicketId(ticketId: number): Observable<TicketMessage[]> {
+    return this.http.get<TicketMessage[]>(this.url + '/tickets/' + ticketId + '/messages', this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error(
+            'TicketMessageService.getMessagesByTicketId(): error retrieving ticket messages: ' + err
+          )
+        );
+      })
+    )
+  }
+
+  createTicketMessageByTicketId(ticketId: number, ticketMessage: TicketMessage): Observable<TicketMessage> {
+    return this.http.post<TicketMessage>(this.url + '/tickets/' + ticketId + '/messages', ticketMessage, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('TicketMessageService.createTicketMessageByTicketId(): error creating ticket message: ' + err)
+        );
+      })
+    )
+  }
+
 }
