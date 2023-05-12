6915a8a9ef5a760e4a914a05e6898abd84a5426f
Michael Harrington <mdharr7@gmail.com>
2023-05-12 09:45:16 +0800

    Successfully retrieve tickets in ticket list component and populate angular material table

--- ngLoreHunter/src/app/shared/components/ticket-list/ticket-list.component.ts
+++ ngLoreHunter/src/app/shared/components/ticket-list/ticket-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Ticket } from 'src/app/models/ticket';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-ticket-list',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./ticket-list.component.css']
 })
 export class TicketListComponent {
+
+  displayedColumns: string[] = ['title', 'status', 'priority', 'createdAt', 'updatedAt', 'user'];
+  dataSource: Ticket[] = [];
+
+  constructor(private ticketService: TicketService) { }
+
+  ngOnInit(): void {
+    this.ticketService.getAllTickets().subscribe(tickets => {
+      this.dataSource = tickets;
+    });
+  }
 
 }
