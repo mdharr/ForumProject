84fa3c813886bbc05a07ae4297f88ab2d2036865
Michael Harrington <mdharr7@gmail.com>
2023-05-21 12:02:07 +0800

    Implement Principal for authentication and refactor post and delete mappings to follow and unfollow a user

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/UserFollowerController.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/UserFollowerController.java
@@ -1,18 +1,28 @@
 package com.skilldistillery.lorehunter.controllers;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.lorehunter.entities.Ticket;
+import com.skilldistillery.lorehunter.entities.User;
 import com.skilldistillery.lorehunter.entities.UserFollower;
 import com.skilldistillery.lorehunter.services.UserFollowerService;
+import com.skilldistillery.lorehunter.services.UserService;
 
 @CrossOrigin({ "*", "http://localhost/"})
 @RestController
@@ -21,15 +31,44 @@ public class UserFollowerController {
 	
     @Autowired
     private UserFollowerService userFollowerService;
+    
+    @Autowired
+    private UserService userService;
 
     @PostMapping("users/{followerId}/follow/{followedId}")
-    public UserFollower followUser(@PathVariable int followerId, @PathVariable int followedId) {
-        return userFollowerService.followUser(followerId, followedId);
+    public ResponseEntity<UserFollower> followUser(Principal principal,
+                                                    @PathVariable int followerId, @PathVariable int followedId) {
+        String authenticatedUsername = principal.getName();
+        User authenticatedUser = userService.showByUsername(authenticatedUsername);
+        
+        if (authenticatedUser != null) {
+            if (authenticatedUser.getId() != followerId) {
+    			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            
+            UserFollower userFollower = userFollowerService.followUser(followerId, followedId);
+            return new ResponseEntity<>(userFollower, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("users/{followerId}/unfollow/{followedId}")
-    public boolean unfollowUser(@PathVariable int followerId, @PathVariable int followedId) {
-        return userFollowerService.unfollowUser(followerId, followedId);
+    public ResponseEntity<Boolean> unfollowUser(Principal principal,
+                                                 @PathVariable int followerId, @PathVariable int followedId) {
+        String authenticatedUsername = principal.getName();
+        User authenticatedUser = userService.showByUsername(authenticatedUsername);
+        
+        if (authenticatedUser != null) {
+            if (authenticatedUser.getId() != followerId) {
+    			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            
+            boolean isUnfollowed = userFollowerService.unfollowUser(followerId, followedId);
+            return new ResponseEntity<>(isUnfollowed, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     // Postman test success
