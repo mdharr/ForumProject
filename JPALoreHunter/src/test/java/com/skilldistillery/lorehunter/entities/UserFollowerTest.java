292805d96ba79d616fe888836563416fec28a750
Michael Harrington <mdharr7@gmail.com>
2023-05-21 10:49:10 +0800

    Create user follower junit test case to test relationship with user entity. Test success

---
+++ JPALoreHunter/src/test/java/com/skilldistillery/lorehunter/entities/UserFollowerTest.java
@@ -0,0 +1,55 @@
+package com.skilldistillery.lorehunter.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserFollowerTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private UserFollower userFollower;
+	private UserFollowerId userFollowerId;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPALoreHunter");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		userFollowerId = new UserFollowerId(1, 2); // Example ID values
+		userFollower = em.find(UserFollower.class, userFollowerId);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		userFollower = null;
+	}
+
+    @Test
+    void test_Follower_entity_mapping() {
+        assertNotNull(userFollower);
+        assertNotNull(userFollower.getFollower());
+        assertNotNull(userFollower.getFollowed());
+        assertEquals("Jill", userFollower.getFollower().getUsername());
+        assertEquals("Wander", userFollower.getFollowed().getUsername());
+    }
+	
+
+}
