f11efb724751c0df3fab4714c9cd08c3bd04bfe7
Michael Harrington <mdharr7@gmail.com>
2023-05-21 11:36:56 +0800

    Create user follower controller along with necessary rest api endpoints for performing basic crud operations

---
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/controllers/UserFollowerController.java
@@ -0,0 +1,45 @@
+package com.skilldistillery.lorehunter.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.lorehunter.entities.UserFollower;
+import com.skilldistillery.lorehunter.services.UserFollowerService;
+
+@CrossOrigin({ "*", "http://localhost/"})
+@RestController
+@RequestMapping("api")
+public class UserFollowerController {
+	
+    @Autowired
+    private UserFollowerService userFollowerService;
+
+    @PostMapping("users/{followerId}/follow/{followedId}")
+    public UserFollower followUser(@PathVariable int followerId, @PathVariable int followedId) {
+        return userFollowerService.followUser(followerId, followedId);
+    }
+
+    @DeleteMapping("users/{followerId}/unfollow/{followedId}")
+    public boolean unfollowUser(@PathVariable int followerId, @PathVariable int followedId) {
+        return userFollowerService.unfollowUser(followerId, followedId);
+    }
+
+    @GetMapping("users/{userId}/followers")
+    public List<UserFollower> getFollowersByUserId(@PathVariable int userId) {
+        return userFollowerService.getFollowersByUserId(userId);
+    }
+
+    @GetMapping("users/{userId}/followed")
+    public List<UserFollower> getFollowedByUserId(@PathVariable int userId) {
+        return userFollowerService.getFollowedByUserId(userId);
+    }
+
+}
