784d0dea8c10889d0d355ecb4ff2e824724e900a
Michael Harrington <mdharr7@gmail.com>
2023-05-28 10:51:10 +0800

    Stub out user game interface and import game interface to inject in constructor

--- ngLoreHunter/src/app/models/user-game.ts
+++ ngLoreHunter/src/app/models/user-game.ts
@@ -1,2 +1,23 @@
+import { Game } from "./game";
+
 export class UserGame {
+  id: {
+    userId: number;
+    gameId: number;
+  }
+  game: Game;
+  category: string;
+  rating: string;
+
+  constructor(
+    id: { userId: number, gameId: number },
+    game: Game = new Game(),
+    category: string = '',
+    rating: string = ''
+  ) {
+  this.id = id;
+  this.game = game;
+  this.category = category;
+  this.rating = rating;
+  }
 }
