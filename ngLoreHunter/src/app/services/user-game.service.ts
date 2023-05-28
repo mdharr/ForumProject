88ce5e02e7d0ef1babb283c787a39c8924736f8f
Michael Harrington <mdharr7@gmail.com>
2023-05-28 11:25:17 +0800

    Stub out method in user game service to fetch a users usergames by userId from backend rest api

--- ngLoreHunter/src/app/services/user-game.service.ts
+++ ngLoreHunter/src/app/services/user-game.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { UserGame } from '../models/user-game';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserGameService {
+  private url = environment.baseUrl + 'api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchUserGames(userId: number): Observable<UserGame[]> {
+    return this.http.get<UserGame[]>(this.url + '/users/' + userId + '/usergames').pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error(
+              'UserGameService.fetchUserGames(): error retrieving user games: ' + err
+            )
+        );
+      })
+    );
+  }
 }
