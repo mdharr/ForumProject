14de8355bf73234b14b89bdb4d7d685064fe9bb7
Michael Harrington <mdharr7@gmail.com>
2023-05-28 11:00:56 +0800

    Generate new games library component for retrieving all games in app library through frontend game service which calls backend rest api for game data and render data to ui through games library component

---
+++ ngLoreHunter/src/app/shared/components/games-library/games-library.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games-library',
+  templateUrl: './games-library.component.html',
+  styleUrls: ['./games-library.component.css']
+})
+export class GamesLibraryComponent {
+
+}
