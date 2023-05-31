a70ea8c29ecc9e51a0adeb572f12ecc0d4ccef23
Michael Harrington <mdharr7@gmail.com>
2023-05-31 09:54:13 +0800

    Implement pagination to posts list component that renders latest posts

--- ngLoreHunter/src/app/shared/components/posts-list/posts-list.component.ts
+++ ngLoreHunter/src/app/shared/components/posts-list/posts-list.component.ts
@@ -11,6 +11,7 @@ import { catchError, EMPTY, map, Observable, Subscription, tap, throwError } fro
 import { HomeService } from 'src/app/services/home.service';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material/dialog';
+import { JumpToPageDialogComponent } from '../jump-to-page-dialog/jump-to-page-dialog.component';
 
 @Component({
   selector: 'app-posts-list',
@@ -23,6 +24,8 @@ export class PostsListComponent implements OnInit, OnDestroy {
 
   @ViewChild('ckeditorInstance') ckeditorInstance: any; // Add this line to access the CKEditor instance
   @ViewChild('filterDialog') filterDialog!: TemplateRef<any>;
+
+  isLoading: boolean = false;
 
   currentPage: number = 1;
   pageSize: number = 10;
@@ -63,6 +66,7 @@ export class PostsListComponent implements OnInit, OnDestroy {
   private loggedInUserSubscription: Subscription | undefined;
   private indexAllSubscription: Subscription | undefined;
   private homeServIndexSubscription: Subscription | undefined;
+  private totalPostsByCategorySubscription: Subscription | undefined;
 
   constructor(
     private postService: PostService,
@@ -103,6 +107,42 @@ export class PostsListComponent implements OnInit, OnDestroy {
       }
     });
 
+    this.loadPostsForPage(this.currentPage);
+
+    this.totalPostsByCategorySubscription = this.postService.getTotalPostsCount().subscribe(totalPosts => {
+      this.totalPosts = totalPosts;
+    });
+
+  }
+
+  ngAfterViewInit() {
+    const toolbarElement = document.querySelector('.ck-toolbar');
+
+    if (toolbarElement) {
+      (toolbarElement as HTMLElement).style.backgroundColor = 'red !important'; // Replace with your desired background color
+    }
+
+    const options = {
+      root: null, // Use the viewport as the root
+      rootMargin: '0px',
+      threshold: 0.5, // Adjust as needed
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // Trigger the method to load posts for the current page asynchronously
+          this.loadPostsForPage(this.currentPage);
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    const paginationElement = document.querySelector('.page-navigation');
+    if (paginationElement) {
+      observer.observe(paginationElement);
+    }
+
   }
 
   ngOnDestroy() {
@@ -133,6 +173,10 @@ export class PostsListComponent implements OnInit, OnDestroy {
           console.error(nojoy);
       }
     });
+  }
+
+  getTotalPages(): number {
+    return Math.ceil(this.totalPosts / this.pageSize);
   }
 
   openFilterDialog() {
@@ -213,6 +257,132 @@ export class PostsListComponent implements OnInit, OnDestroy {
       })
     ).subscribe(categoryId => {
       this.navigateToComments(categoryId, postId);
+    });
+  }
+
+  generatePageArray() {
+    const maxVisiblePages = 5; // Maximum number of visible pages in the navigation
+
+    if (this.totalPages <= maxVisiblePages) {
+      // If total pages is less than or equal to maxVisiblePages, display all pages
+      this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+    } else {
+      const firstPage = 1;
+      const lastPage = this.totalPages;
+      const currentPage = this.currentPage;
+
+      // Add first page
+      const pageNumbers: number[] = [firstPage];
+
+      // Add ellipsis if current page is not within the first 3 pages
+      if (currentPage > 3) {
+        pageNumbers.push(-1);
+      }
+
+      // Add visible page numbers
+      const startPage = Math.max(2, currentPage - 2);
+      const endPage = Math.min(lastPage - 1, currentPage + 2);
+      for (let i = startPage; i <= endPage; i++) {
+        pageNumbers.push(i);
+      }
+
+      // Add ellipsis if current page is not within the last 3 pages
+      if (currentPage < lastPage - 2) {
+        pageNumbers.push(-1);
+      }
+
+      // Add last page
+      pageNumbers.push(lastPage);
+
+      this.pages = pageNumbers;
+    }
+    this.isLoading = false;
+  }
+
+  // Modify the goToPage method
+  async goToPage(page: number): Promise<void> {
+    if (page >= 1 && page <= this.totalPages) {
+      this.isLoading = true;
+      this.currentPage = page;
+      this.generatePageArray();
+      // Call a method to retrieve the posts for the current page
+      await this.loadPostsForPage(page);
+      this.isLoading = false;
+    }
+  }
+
+  loadPostsForPage(page: number): void {
+    this.isLoading = true;
+    const startIndex = (page - 1) * this.pageSize;
+    let endIndex = startIndex + this.pageSize;
+
+    this.posts$ = this.postService.getAllPosts().pipe(
+      map(posts => {
+        const sortedPosts = posts
+          .sort((a, b) => {
+            let lastEditedComparison = new Date(b.lastComment).getTime() - new Date(a.lastComment).getTime();
+            if (lastEditedComparison === 0) {
+              return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+            } else {
+              return lastEditedComparison;
+            }
+          });
+
+        const totalPosts = sortedPosts.length;
+        const totalPages = this.getTotalPages();
+        const remainingPosts = totalPosts - startIndex;
+
+        if (remainingPosts < this.pageSize) {
+          endIndex = startIndex + remainingPosts;
+        }
+
+        const slicedPosts = sortedPosts.slice(startIndex, endIndex);
+
+        this.totalPosts = totalPosts;
+        this.totalPages = totalPages;
+        this.generatePageArray();
+
+        return slicedPosts;
+      })
+    );
+
+    this.posts$.subscribe(slicedPosts => {
+      this.posts = slicedPosts;
+      this.isLoading = false;
+    });
+  }
+
+  goToPreviousPage(): void {
+    if (this.currentPage > 1) {
+      this.goToPage(this.currentPage - 1);
+    }
+  }
+
+  goToNextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.goToPage(this.currentPage + 1);
+    }
+  }
+
+  onJumpToPage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.goToPage(page);
+    }
+  }
+
+  onJumpToPageDialog() {
+    console.log('onJumpToPageDialog called');
+
+    const dialogRef = this.dialog.open(JumpToPageDialogComponent, {
+      width: '250px',
+      data: { currentPage: this.currentPage, totalPages: this.totalPages } // Pass current page and total pages as data to the dialog
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result && result.page) {
+        const page = result.page;
+        this.onJumpToPage(page); // Call the onJumpToPage method with the selected page number
+      }
     });
   }
 
