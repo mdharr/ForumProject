d90c90e4f8a9d3208ed64b78bbe287e93a0effbc
Michael Harrington <mdharr7@gmail.com>
2023-06-10 13:17:33 +0800

    Stub out user conversation junit test case and test entity mapping

--- JPALoreHunter/src/test/java/com/skilldistillery/lorehunter/entities/UserConversationTest.java
+++ JPALoreHunter/src/test/java/com/skilldistillery/lorehunter/entities/UserConversationTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -15,26 +16,34 @@ class UserConversationTest {
 	
 	private static EntityManagerFactory emf;
 	private EntityManager em;
+	private UserConversation userConversation;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPALoreHunter");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		userConversation = em.find(UserConversation.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		userConversation = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_UserConversation_entity_mapping() {
+		assertNotNull(userConversation);
+		assertEquals("Jill", userConversation.getUser().getUsername());
 	}
 
 }
