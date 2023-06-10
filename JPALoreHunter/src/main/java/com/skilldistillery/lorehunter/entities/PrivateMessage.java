5127cb60b4940a11dc1f78767f1d56ad47e9d831
Michael Harrington <mdharr7@gmail.com>
2023-06-10 12:47:29 +0800

    Add fields to private message entity, generate constructors, getters/setters, hashcode/equals, and to string

--- JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/PrivateMessage.java
+++ JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/PrivateMessage.java
@@ -1,10 +1,20 @@
 package com.skilldistillery.lorehunter.entities;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Table(name = "private_message")
@@ -13,5 +23,109 @@ public class PrivateMessage {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String content;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@JsonBackReference
+	@ManyToOne
+	@JoinColumn(name = "user_conversation_id")
+	private UserConversation userConversation;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public PrivateMessage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrivateMessage(int id, String content, LocalDateTime createdAt, UserConversation userConversation,
+			User user) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.createdAt = createdAt;
+		this.userConversation = userConversation;
+		this.user = user;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public UserConversation getUserConversation() {
+		return userConversation;
+	}
+
+	public void setUserConversation(UserConversation userConversation) {
+		this.userConversation = userConversation;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PrivateMessage other = (PrivateMessage) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("PrivateMessage [id=");
+		builder.append(id);
+		builder.append(", content=");
+		builder.append(content);
+		builder.append(", createdAt=");
+		builder.append(createdAt);
+		builder.append(", userConversation=");
+		builder.append(userConversation);
+		builder.append(", user=");
+		builder.append(user);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
--- JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/UserConversation.java
+++ JPALoreHunter/src/main/java/com/skilldistillery/lorehunter/entities/UserConversation.java
@@ -1,10 +1,24 @@
 package com.skilldistillery.lorehunter.entities;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "user_conversation")
@@ -13,5 +27,21 @@ public class UserConversation {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+	
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@JsonManagedReference
+	@OneToMany(mappedBy = "userConversation", cascade = CascadeType.ALL)
+	private List<PrivateMessage> privateMessages = new ArrayList<>();
 
 }
