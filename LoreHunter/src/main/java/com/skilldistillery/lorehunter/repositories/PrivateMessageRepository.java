b6bab1975a579a86efaf7f44d3c094b3bb7f1ee1
Michael Harrington <mdharr7@gmail.com>
2023-06-21 14:35:14 +0800

    Modify variable name for int parameter in private message repository method that retrieves a list of private messages by user conversation id

--- LoreHunter/src/main/java/com/skilldistillery/lorehunter/repositories/PrivateMessageRepository.java
+++ LoreHunter/src/main/java/com/skilldistillery/lorehunter/repositories/PrivateMessageRepository.java
@@ -9,11 +9,13 @@ import com.skilldistillery.lorehunter.entities.UserConversation;
 
 public interface PrivateMessageRepository extends JpaRepository<PrivateMessage, Integer> {
 	
-	List<PrivateMessage> findByUserConversationIdOrderByCreatedAtAsc(int ticketId);
+	List<PrivateMessage> findByUserConversationIdOrderByCreatedAtAsc(int userConversationid);
     
     List<PrivateMessage> findBySenderIdOrderByCreatedAtAsc(int userId);
     
     List<PrivateMessage> findByUserConversationIdAndSenderIdOrderByCreatedAtAsc(int userConversationId, int userId);
+    
+    List<PrivateMessage> findByUserConversationIdAndRecipientIdOrderByCreatedAtAsc(int userConversationId, int userId);
     
     List<PrivateMessage> findByUserConversation(UserConversation userConversation);
 
